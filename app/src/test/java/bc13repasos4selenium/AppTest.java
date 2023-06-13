7cec47468844992c0ff1468f1a0c638031b7868e
Cristian Vargas <cristian.vargas@tsoftglobal.com>
2023-06-13 13:03:18 -0300

    Se actualizo las librerias en el proyecto

--- app/src/test/java/bc13repasos4selenium/AppTest.java
+++ app/src/test/java/bc13repasos4selenium/AppTest.java
@@ -11,4 +11,7 @@ class AppTest {
         App classUnderTest = new App();
         assertNotNull(classUnderTest.getGreeting(), "app should have a greeting");
     }
+    @Test void ejemploLevantarBrowser(){
+        WebDriverManager.chromedriver().setup();
+    }
 }
